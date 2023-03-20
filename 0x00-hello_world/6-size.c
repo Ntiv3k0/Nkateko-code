240321be644f1ee5760e8e125c3a1dd27da98742
Nkateko-Code <vivid.hlatshwayo@yahoo.com>
2023-03-20 15:21:06 -0700

    new size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - print the size of various typs based on computer
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	printf("size of a char: %lu byte(s)\n", sizeof(char));
+	printf("SIze of an int: %lu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %lu byte(s\n", sizeof(long int));
+	print("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
+	printf("Size of a float %lu byte(s)\n",sizeof(float));
+return (0);
+}
