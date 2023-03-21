d5139c04826a4a738d4ddc5d783ea3e845ec83c0
Nkateko-Code <vivid.hlatshwayo@yahoo.com>
2023-03-21 15:03:17 -0700

    2-print_alphabte_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,10 +1,12 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - function that print alphabet 10 time
+ * print_alphabet_x10 - function that print alphabet 10x time
  *
  * Return: always 0
  */
+
+
 void print_alphabet_x10(void)
 {
 	int i;
@@ -13,7 +15,7 @@ void print_alphabet_x10(void)
 	for (i = 1 ; i <= 10 ; i++)
 	{
 		for (j = 'a' ; j <= 'z' ; j++)
-			_putchar('\n');
+			_putchar(j);
 		_putchar('\n');
 	}
 }
