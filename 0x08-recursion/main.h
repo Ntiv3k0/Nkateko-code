a44ec6998fb3771147d2a4bb5d4d0eabc66cbd98
Nkateko-Code <vivid.hlatshwayo@yahoo.com>
2023-04-05 18:55:35 -0700

    amanded

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -2,7 +2,6 @@
 #define MAIN_H
 
 void _puts_recursion(char *s);
-int _putchar(char c);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
 int factorial(int n);
@@ -10,5 +9,6 @@ int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int is_palindrome(char *s);
+int _putchar(char c);
 #endif
 
