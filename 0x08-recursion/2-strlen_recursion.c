0efba0454fd8479968a6f403309578c0a668a7cf
Nkateko-Code <vivid.hlatshwayo@yahoo.com>
2023-04-05 19:32:38 -0700

    2-rev

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
- * _strlen_recursion - Returns the lengh of a string.
+ * _strlen_recursion - Returns the length of a string.
  * @s: The string to be measured.
  *
- * Return: The lengh of the string.
+ * Return: The length of the string.
  */
 
-int _strlen_recursion(char*s)
+int _strlen_recursion(char *s)
 {
 	int longit = 0;
 
