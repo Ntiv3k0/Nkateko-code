47fc67050dbfe4b0c2a4df1f5646a55a0b6bde4a
Nkateko-Code <vivid.hlatshwayo@yahoo.com>
2023-04-05 18:44:09 -0700

    1-print_rev_recursion.c

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,13 @@
+#include "main"
+/**
+ * _print_rev_recursion - prints string in reverse.
+ * @s: The string to be printed.
+ */
+void_print_rev_recursion(char*s)
+{
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar (*s);
+	}
+}
