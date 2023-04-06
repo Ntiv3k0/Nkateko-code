b1f6f9d92529a53e9f3775b3303df107e9e3082f
Nkateko-Code <vivid.hlatshwayo@yahoo.com>
2023-04-05 18:39:03 -0700

    fixed

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * _puts_recursion-function like puts();
+ * _puts_recursion-function like puts ();
  * @s: input
  * Return: always 0 (Success)
  */
 void _puts_recursion(char *s)
 {
-	if(*s)
+	if (*s)
 	{
 		_putchar(*s);
 		_puts_recursion(s + 1);
