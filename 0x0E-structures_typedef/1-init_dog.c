1aa44e0cf4bb28e6ab1b57c152f5b49d6d99f1e5
Nkateko-Code <vivid.hlatshwayo@yahoo.com>
2023-04-17 19:51:02 -0700

    0x0E. C - Structures, typedef

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,5 +1,5 @@
 #include <stdlib.h>
-	#include "dog.h"
+#include "dog.h"
 
 
 	/**
@@ -9,11 +9,11 @@
 	 * @age: age to initialize
 	 * @owner: owner to initialize
 	 */
-	void init_dog(struct dog *d, char *name, float age, char *owner)
-	{
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
 		if (d == NULL)
 			d = malloc(sizeof(struct dog));
 		d->name = name;
 		d->age = age;
 		d->owner = owner;
-	}
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -22,7 +22,6 @@
  */
 typedef struct dog dog_t;
 
-
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
@@ -30,5 +29,4 @@ void free_dog(dog_t *d);
 char *_strcpy(char *dest, char *src);
 int _strlen(char *s);
 
-
-	#endif
+#endif
