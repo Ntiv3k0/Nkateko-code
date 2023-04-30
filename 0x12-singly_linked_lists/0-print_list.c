d5631999bacdf638d1d4a27e387d2b763ade56e3
Nkateko-Code <vivid.hlatshwayo@yahoo.com>
2023-04-29 17:25:11 -0700

    amends

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -3,21 +3,21 @@
 
 /**
  * print_list - produce all the elements of a linked list
- * @p: pointer to the list_t list to produce
+ * @h: pointer to the list_t list to produce
  * Return: amount of nodes printed
  */
 
-size_t print_list(const list_t *p)
+size_t print_list(const list_t *h)
 {
 	size_t n = 0;
 
-	while (p)
+	while (h)
 	{
 		if (!p->sty)
 			printf("[0](nil)\n";)
 		else
-		 printf("[%u] %s\n", p->len, p->str);
-	p = p->next;
+		 printf("[%u] %s\n", h->len, p->str);
+	h = h->next;
 	s++;
 	}
 	return (s);
