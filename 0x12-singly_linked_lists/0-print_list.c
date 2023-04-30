d09cf7640dc0a31baef0d14485cee1d2f0216d68
Nkateko-Code <vivid.hlatshwayo@yahoo.com>
2023-04-29 17:44:37 -0700

    fixed

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,16 +9,16 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t n = 0;
+	size_t S = 0;
 
 	while (h)
 	{
-		if (!p->sty)
-			printf("[0](nil)\n";)
+		if (!h->str)
+			printf("[0](nil)\n");
 		else
-		 printf("[%u] %s\n", h->len, p->str);
+		 printf("[%u] %s\n", h->len, h->str);
 	h = h->next;
-	s++;
+	S++;
 	}
-	return (s);
+	return (S);
 }
