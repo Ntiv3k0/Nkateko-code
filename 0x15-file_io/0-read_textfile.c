7cb2a05389943bee24be57eb7f59ff7ea4e063c3
Nkateko-Code <vivid.hlatshwayo@yahoo.com>
2023-05-09 15:39:10 -0700

    first

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * read_textfile - Read text file print to STDOUT
+ * @filename: The file that is being read
+ * @letters: amount of letters to be read
+ * Return: W - The amount of bytes read and printed
+ *     0 when the function fails of filename is NULL
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *buf;
+	ssize_t fd, w, t;
+
+	fd = open(filename, O_RDONLY);
+
+	if (fd == -1)
+	{
+	perror("Could not open file");
+	return (0);
+	}
+
+	buf = malloc(sizeof(char) * letters);
+	t =read(fd, buf, letters);
+	w = write(STDOUT_FILENO, buf, t);
+
+	free(buf);
+	close(fd);
+	return(w);
+}
