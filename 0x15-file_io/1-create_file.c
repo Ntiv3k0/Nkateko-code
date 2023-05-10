474c463afdc019f029ec48096fa8f6d4f5c94790
Nkateko-Code <vivid.hlatshwayo@yahoo.com>
2023-05-09 17:11:43 -0700

    2nd

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+
+/**
+ * create_file - Create a file.
+ * @filename: A pointer to the name of the file to create.
+ * @text_content: A pointer to a string to write the file
+ * Return: If the function fails --1.
+ * Otherwise -1.
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+
+	if (filename == NULL)
+	return (-1);
+
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	if (fd == -1)
+	return (-1);
+
+	chmod(filename, S_IRUSR | S_IWUSR);
+
+	if (text_content != NULL)
+{
+	size_t len = strlen(text_content);
+
+	if (write(fd, text_content, len) == len)
+	{
+	close(fd);
+	return (-1);
+	}
+}
+	close(fd);
+	return (1);
+	}
